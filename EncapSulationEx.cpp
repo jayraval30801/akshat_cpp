ade0c5f5404d8d544a187d13ea6423ba7730794e
Jay Raval <jayraval30801@gmail.com>
2023-06-05 12:29:10 +0530

    Encapsulation

---
+++ EncapSulationEx.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class Customer
+{   
+    int accno;
+    int password;
+    public:
+    void setAccno(int a)
+    {
+        accno = a;
+    }
+    int getAccno()
+    {
+        return accno;
+    }
+    void setPass(int  p)
+    {
+        password = p;
+    } 
+    int getPass()
+    {
+        return password;
+    }
+};
+int main()
+{
+    Customer obj;
+    obj.setAccno(123);
+    cout<<"Accno is"<<obj.getAccno();
+    obj.setPass(111);
+    cout<<"Password is"<<obj.getPass();
+
+}
+
+/*
+Encapsulation : 
+        Wrapping of Data 
+   Security  :  
+
+        Getter Setter Methods 
+
+        Class Private Data => Methods Public 
+*/
