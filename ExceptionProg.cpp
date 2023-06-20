c63e78c24ae6360889d6d9b80cbcea5579caf9fa
Jay Raval <jayraval30801@gmail.com>
2023-06-20 11:23:35 +0530

    Exception Hand

---
+++ ExceptionProg.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,p;
+    try
+    {
+    cout<<"Enter a";
+    cin>>a;
+
+    cout<<"Enter b";
+    cin>>b;
+
+    if(b==0)
+    {
+        throw "Error Not Divisible by 0";
+    }
+    else
+    {
+    p = a/b;
+    cout<<"Value of p is "<<p;
+    }
+    }catch(const char *msg)//string *ptr = ptr[i];
+    {
+        cout<<msg;
+    }
+}
+/*
+Can not divide by 0 
+
+*/
